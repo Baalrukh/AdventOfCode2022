f28c9fbc270931db01e12d55ec9b71c9eed3fb30
Baalrukh <baalrukh@gmail.com>
2023-01-08 22:52:15 +0100

    Day 22 part 2

--- AdventOfCode2022.Test/Day22Tests.cs
+++ AdventOfCode2022.Test/Day22Tests.cs
@@ -22,6 +22,17 @@ public class Day22Tests
         "",
         "10R5L5R10L4R5L5",
     };
+    
+    private Dictionary<(IntVector2, IntVector2), Day22.PositionTransformersParameters> _positionTransformersParameters = new()
+    {
+        { (new(2, 0), new(2,  1)), new(new(2, 1), Day22.Direction.Bottom, false) },
+        { (new(2, 1), new(3,  1)), new(new(3, 2), Day22.Direction.Bottom, true) },
+        { (new(3, 2), new(2,  2)), new(new(2, 2), Day22.Direction.Left, false) },
+        { (new(2, 2), new(2,  3)), new(new(0, 1), Day22.Direction.Top, true) },
+        { (new(0, 1), new(1,  1)), new(new(1, 1), Day22.Direction.Right, false) },
+        { (new(1, 1), new(1,  0)), new(new(2, 0), Day22.Direction.Right, false) },
+        // other transitions are not used
+    };
 
     [Test]
     public void TestPart1()
@@ -32,7 +43,7 @@ public class Day22Tests
     [Test]
     public void TestPart2()
     {
-        Assert.AreEqual(5031, new Day22().ExecutePart2(_sampleLines));
+        Assert.AreEqual(5031, Day22.GetCubePassword(_sampleLines, 4, _positionTransformersParameters));
     }
 
     [Test]
@@ -49,63 +60,63 @@ public class Day22Tests
     public void TestAdvanceIfNecessary_Distance0()
     {
         Day22.World world = Day22.World.Parse(_sampleLines.Take(_sampleLines.Length - 2).ToArray(), 4);
-        Assert.AreEqual(new IntVector2(8, 0), world.AdvanceIfNecessaryWithWrapping(new IntVector2(8, 0), Day22.Direction.Right, 0));
+        Assert.AreEqual(new IntVector2(8, 0), world.AdvanceIfNecessaryWithWrapping(new IntVector2(8, 0), Day22.Direction.Right, 0).Item1);
     }
 
     [Test]
     public void TestAdvanceIfNecessary_Simple_Right()
     {
         Day22.World world = Day22.World.Parse(_sampleLines.Take(_sampleLines.Length - 2).ToArray(), 4);
-        Assert.AreEqual(new IntVector2(10, 0), world.AdvanceIfNecessaryWithWrapping(new IntVector2(8, 0), Day22.Direction.Right, 2));
+        Assert.AreEqual(new IntVector2(10, 0), world.AdvanceIfNecessaryWithWrapping(new IntVector2(8, 0), Day22.Direction.Right, 2).Item1);
     }
 
     [Test]
     public void TestAdvanceIfNecessary_Simple_Bottom()
     {
         Day22.World world = Day22.World.Parse(_sampleLines.Take(_sampleLines.Length - 2).ToArray(), 4);
-        Assert.AreEqual(new IntVector2(8, 1), world.AdvanceIfNecessaryWithWrapping(new IntVector2(8, 0), Day22.Direction.Bottom, 1));
+        Assert.AreEqual(new IntVector2(8, 1), world.AdvanceIfNecessaryWithWrapping(new IntVector2(8, 0), Day22.Direction.Bottom, 1).Item1);
     }
 
     [Test]
     public void TestAdvanceIfNecessary_Wall()
     {
         Day22.World world = Day22.World.Parse(_sampleLines.Take(_sampleLines.Length - 2).ToArray(), 4);
-        Assert.AreEqual(new IntVector2(10, 0), world.AdvanceIfNecessaryWithWrapping(new IntVector2(8, 0), Day22.Direction.Right, 10));
+        Assert.AreEqual(new IntVector2(10, 0), world.AdvanceIfNecessaryWithWrapping(new IntVector2(8, 0), Day22.Direction.Right, 10).Item1);
     }
 
     [Test]
     public void TestAdvanceIfNecessary_WarpingRight()
     {
         Day22.World world = Day22.World.Parse(_sampleLines.Take(_sampleLines.Length - 2).ToArray(), 4);
-        Assert.AreEqual(new IntVector2(0, 5), world.AdvanceIfNecessaryWithWrapping(new IntVector2(11, 5), Day22.Direction.Right, 1));
+        Assert.AreEqual(new IntVector2(0, 5), world.AdvanceIfNecessaryWithWrapping(new IntVector2(11, 5), Day22.Direction.Right, 1).Item1);
     }
     
     [Test]
     public void TestAdvanceIfNecessary_WarpingRightIntoWall()
     {
         Day22.World world = Day22.World.Parse(_sampleLines.Take(_sampleLines.Length - 2).ToArray(), 4);
-        Assert.AreEqual(new IntVector2(11, 2), world.AdvanceIfNecessaryWithWrapping(new IntVector2(9, 2), Day22.Direction.Right, 10));
+        Assert.AreEqual(new IntVector2(11, 2), world.AdvanceIfNecessaryWithWrapping(new IntVector2(9, 2), Day22.Direction.Right, 10).Item1);
     }
 
     [Test]
     public void TestAdvanceIfNecessary_WarpingLeft()
     {
         Day22.World world = Day22.World.Parse(_sampleLines.Take(_sampleLines.Length - 2).ToArray(), 4);
-        Assert.AreEqual(new IntVector2(11, 5), world.AdvanceIfNecessaryWithWrapping(new IntVector2(0, 5), Day22.Direction.Left, 1));
+        Assert.AreEqual(new IntVector2(11, 5), world.AdvanceIfNecessaryWithWrapping(new IntVector2(0, 5), Day22.Direction.Left, 1).Item1);
     }
 
     [Test]
     public void TestAdvanceIfNecessary_WarpingTop()
     {
         Day22.World world = Day22.World.Parse(_sampleLines.Take(_sampleLines.Length - 2).ToArray(), 4);
-        Assert.AreEqual(new IntVector2(5, 7), world.AdvanceIfNecessaryWithWrapping(new IntVector2(5, 4), Day22.Direction.Top, 1));
+        Assert.AreEqual(new IntVector2(5, 7), world.AdvanceIfNecessaryWithWrapping(new IntVector2(5, 4), Day22.Direction.Top, 1).Item1);
     }
 
     [Test]
     public void TestAdvanceIfNecessary_WarpingBottom()
     {
         Day22.World world = Day22.World.Parse(_sampleLines.Take(_sampleLines.Length - 2).ToArray(), 4);
-        Assert.AreEqual(new IntVector2(5, 4), world.AdvanceIfNecessaryWithWrapping(new IntVector2(5, 7), Day22.Direction.Bottom, 1));
+        Assert.AreEqual(new IntVector2(5, 4), world.AdvanceIfNecessaryWithWrapping(new IntVector2(5, 7), Day22.Direction.Bottom, 1).Item1);
     }
 
 }
--- AdventOfCode2022/Day22.cs
+++ AdventOfCode2022/Day22.cs
@@ -1,9 +1,101 @@
+using System.Diagnostics;
 using AdventOfCode2022.Utils;
 
 namespace AdventOfCode2022;
 
 public class Day22 : Exercise
 {
+    public struct PositionTransformersParameters
+    {
+        public readonly IntVector2 DestinationFaceIndices;
+        public readonly Direction EntranceDirection;
+        public readonly bool InvertSideCoordinate;
+
+        public PositionTransformersParameters(IntVector2 destinationFaceIndices, Direction entranceDirection, bool invertSideCoordinate)
+        {
+            DestinationFaceIndices = destinationFaceIndices;
+            EntranceDirection = entranceDirection;
+            InvertSideCoordinate = invertSideCoordinate;
+        }
+        
+        
+        public IntVector2 FacePositionTransformer(IntVector2 position, Direction currentDirection, IntVector2 srcFaceIndices, int faceSize)
+        {
+            IntVector2 localPosition = position - srcFaceIndices * faceSize;
+
+            IntVector2 baseDstPosition = DestinationFaceIndices * faceSize;
+
+            int lateralPos;
+            if ((currentDirection == Direction.Right) || (currentDirection == Direction.Left))
+            {
+                lateralPos = localPosition.Y;
+            }
+            else
+            {
+                lateralPos = localPosition.X;
+            }
+
+
+            if (InvertSideCoordinate)
+            {
+                lateralPos = faceSize - lateralPos - 1;
+            }
+        
+            if (EntranceDirection == Direction.Right)
+            {
+                localPosition = new IntVector2(0, lateralPos);
+            }
+            else if (EntranceDirection == Direction.Left)
+            {
+                localPosition = new IntVector2(faceSize - 1, lateralPos);
+            }
+            else if (EntranceDirection == Direction.Top)
+            {
+                localPosition = new IntVector2(lateralPos, faceSize - 1);
+            }
+            else
+            {
+                localPosition = new IntVector2(lateralPos, 0);
+            }
+
+            return localPosition + baseDstPosition;
+        }
+    }
+
+    private Dictionary<(IntVector2, IntVector2), PositionTransformersParameters> _positionTransformersParameters = new()
+    {
+        { (new(1, 0), new(1, -1)), new(new(0, 3), Direction.Right, false) },
+        { (new(1, 0), new(1,  1)), new(new(1, 1), Direction.Bottom, false) },
+        { (new(1, 0), new(0,  0)), new(new(0, 2), Direction.Right, true) },
+        { (new(1, 0), new(2,  0)), new(new(2, 0), Direction.Right, false) },
+
+        { (new(2, 0), new(2, -1)), new(new(0, 3), Direction.Top, false) },
+        { (new(2, 0), new(2,  1)), new(new(1, 1), Direction.Left, false) },
+        { (new(2, 0), new(1,  0)), new(new(1, 0), Direction.Left, false) },
+        { (new(2, 0), new(3,  0)), new(new(1, 2), Direction.Left, true) },
+
+        { (new(1, 1), new(1, 0)), new(new(1, 0), Direction.Top, false) },
+        { (new(1, 1), new(1, 2)), new(new(1, 2), Direction.Bottom, false) },
+        { (new(1, 1), new(0, 1)), new(new(0, 2), Direction.Bottom, false) },
+        { (new(1, 1), new(2, 1)), new(new(2, 0), Direction.Top, false) },
+
+        { (new(0, 2), new(0,  1)), new(new(1, 1), Direction.Right, false) },
+        { (new(0, 2), new(0,  3)), new(new(0, 3), Direction.Bottom, false) },
+        { (new(0, 2), new(-1, 2)), new(new(1, 0), Direction.Right, true) },
+        { (new(0, 2), new(1,  2)), new(new(1, 2), Direction.Right, false) },
+
+        { (new(1, 2), new(1, 1)), new(new(1, 1), Direction.Top, false) },
+        { (new(1, 2), new(1, 3)), new(new(0, 3), Direction.Left, false) },
+        { (new(1, 2), new(0, 2)), new(new(0, 2), Direction.Left, false) },
+        { (new(1, 2), new(2, 2)), new(new(2, 0), Direction.Left, true) },
+
+        { (new(0, 3), new(0,  2)), new(new(0, 2), Direction.Top, false) },
+        { (new(0, 3), new(0,  4)), new(new(2, 0), Direction.Bottom, false) },
+        { (new(0, 3), new(-1, 3)), new(new(1, 0), Direction.Bottom, false) },
+        { (new(0, 3), new(1,  3)), new(new(1, 2), Direction.Top, false) },
+    };
+
+    
     public long ExecutePart1(string[] lines)
     {
         int emptyLineIndex = lines.IndexOf("");
@@ -19,10 +111,13 @@ public class Day22 : Exercise
 
     public long ExecutePart2(string[] lines)
     {
-        int emptyLineIndex = lines.IndexOf("");
-        World world = World.Parse(lines.Take(emptyLineIndex).ToArray(), 50);
-        int startX = Enumerable.Range(0, world.Map.Width).First(x => world.Map[x, 0] == '.');
-        (IntVector2 position, Direction direction) = world.ApplyMotionOnCube(new IntVector2(startX, 0), Direction.Right, lines[emptyLineIndex + 1]);
+        return GetCubePassword(lines, 50, _positionTransformersParameters);
+    }
+
+    public static long GetCubePassword(string[] lines, int faceSize,
+        IReadOnlyDictionary<(IntVector2, IntVector2), PositionTransformersParameters> positionTransformers)
+    {
+        IntVector2 position = GetFinalPositionCube(lines, out Direction direction, faceSize, positionTransformers);
 
         // 1000 * y + 4 * x + d 
         // 1000 * 6 + 4 * 8 + 0
@@ -30,17 +125,30 @@ public class Day22 : Exercise
         return 1000L * (position.Y + 1) + 4L * (position.X + 1) + direction.Index;
     }
 
+    private static IntVector2 GetFinalPositionCube(string[] lines, out Direction direction, int faceSize,
+        IReadOnlyDictionary<(IntVector2, IntVector2), PositionTransformersParameters> positionTransformers)
+    {
+        int emptyLineIndex = lines.IndexOf("");
+        World world = World.Parse(lines.Take(emptyLineIndex).ToArray(), faceSize);
+        int startX = Enumerable.Range(0, world.Map.Width).First(x => world.Map[x, 0] == '.');
+        (var position, direction) =
+            world.ApplyMotionOnCube(new IntVector2(startX, 0), Direction.Right, lines[emptyLineIndex + 1], positionTransformers);
+        return position;
+    }
+
     public class Direction
     {
         public readonly string Name;
         public readonly IntVector2 Offset;
         private readonly int _index;
-
-        public Direction(string name, IntVector2 offset, int index)
+        public readonly char Char;
+        
+        public Direction(string name, IntVector2 offset, int index, char c)
         {
             Name = name;
             Offset = offset;
             _index = index;
+            Char = c;
         }
 
         public int Index => _index;
@@ -60,10 +168,10 @@ public class Day22 : Exercise
             return Name;
         }
 
-        public static Direction Right = new Direction("Right", new IntVector2(1, 0), 0);
-        public static Direction Bottom =  new Direction("Bottom", new IntVector2(0, 1), 1);
-        public static Direction Left = new Direction("Left", new IntVector2(-1, 0), 2);
-        public static Direction Top = new Direction("Top", new IntVector2(0, -1), 3);
+        public static Direction Right = new Direction("Right", new IntVector2(1, 0), 0, '>');
+        public static Direction Bottom =  new Direction("Bottom", new IntVector2(0, 1), 1, 'v');
+        public static Direction Left = new Direction("Left", new IntVector2(-1, 0), 2, '<');
+        public static Direction Top = new Direction("Top", new IntVector2(0, -1), 3, '^');
 
         public static readonly Direction[] Directions = new[]
         {
@@ -73,12 +181,14 @@ public class Day22 : Exercise
 
     public class World
     {
-        public Map2D<char> Map;
-        public Interval[] HorizontalRange;
-        public Interval[] VerticalRange;
-        private int _faceSize;
+        public readonly Map2D<char> Map;
+        public readonly Interval[] HorizontalRange;
+        public readonly Interval[] VerticalRange;
+        private readonly int _faceSize;
 
-        private delegate IntVector2 AdvanceMethod(IntVector2 position, Direction direction, int distance);
+        private delegate (IntVector2, Direction) AdvanceMethod(IntVector2 position, Direction direction, int distance);
+
+        private readonly Dictionary<IntVector2, char> _pathChars = new();
 
         private World(Map2D<char> map, Interval[] horizontalRange, Interval[] verticalRange, int faceSize)
         {
@@ -90,15 +200,16 @@ public class Day22 : Exercise
 
         public (IntVector2, Direction) ApplyMotionWithWraping(IntVector2 startPosition, Direction direction, string sequence)
         {
-            return ApplyMotion(startPosition, direction, sequence, AdvanceIfNecessaryWithWrapping);
+            return ApplyMotion(startPosition, direction, sequence, AdvanceIfNecessaryWithWrapping, false);
         }
 
-        public (IntVector2, Direction) ApplyMotionOnCube(IntVector2 startPosition, Direction direction, string sequence)
+        public (IntVector2, Direction) ApplyMotionOnCube(IntVector2 startPosition, Direction direction, string sequence, 
+            IReadOnlyDictionary<(IntVector2, IntVector2), PositionTransformersParameters> positionTransformers)
         {
-            return ApplyMotion(startPosition, direction, sequence, AdvanceIfNecessaryOnCube);
+            return ApplyMotion(startPosition, direction, sequence, (position, dir, distance) => AdvanceIfNecessaryOnCube(position, dir, distance, positionTransformers), false);
         }
 
-        private (IntVector2, Direction) ApplyMotion(IntVector2 startPosition, Direction direction, string sequence, AdvanceMethod advanceMethod)
+        private (IntVector2, Direction) ApplyMotion(IntVector2 startPosition, Direction direction, string sequence, AdvanceMethod advanceMethod, bool debug)
         {
             IntVector2 position = startPosition;
             int distance = 0;
@@ -106,15 +217,21 @@ public class Day22 : Exercise
             {
                 if (c == 'R')
                 {
-                    position = advanceMethod(position, direction, distance);
+                    (position, direction) = advanceMethod(position, direction, distance);
                     distance = 0;
                     direction = direction.TurnRight();
-                } else if (c == 'L')
+                    if (debug)
+                        PrintMap();
+                }
+                else if (c == 'L')
                 {
-                    position = advanceMethod(position, direction, distance);
+                    (position, direction) = advanceMethod(position, direction, distance);
                     distance = 0;
                     direction = direction.TurnLeft();
-                } else
+                    if (debug)
+                        PrintMap();
+                }
+                else
                 {
                     distance = distance * 10 + c - '0';
                 }
@@ -122,17 +239,45 @@ public class Day22 : Exercise
 
             if (distance > 0)
             {
-                position = advanceMethod(position, direction, distance);
+                (position, direction) = advanceMethod(position, direction, distance);
+                if (debug)
+                    PrintMap();
             }
 
             return (position, direction);
         }
 
-        public IntVector2 AdvanceIfNecessaryWithWrapping(IntVector2 position, Direction direction, int distance)
+        private void PrintMap()
+        {
+            Debug.WriteLine("");
+            
+            char[][] map = Enumerable.Range(0, Map.Height).Select(x => new char[Map.Width]).ToArray();
+
+            for (int y = 0; y < Map.Height; y++)
+            {
+                char[] line = map[y];
+                for (int x = 0; x < Map.Width; x++)
+                {
+                    line[x] = Map[x, y];
+                }
+            }
+
+            foreach (var pair in _pathChars)
+            {
+                map[pair.Key.Y][pair.Key.X] = pair.Value;
+            }
+
+            foreach (char[] chars in map)
+            {
+                Debug.WriteLine(new string(chars, 0, chars.Length));
+            }
+        }
+
+        public (IntVector2, Direction) AdvanceIfNecessaryWithWrapping(IntVector2 position, Direction direction, int distance)
         {
             if (distance == 0)
             {
-                return position;
+                return (position, direction);
             }
 
             for (int i = 0; i < distance; i++)
@@ -163,103 +308,62 @@ public class Day22 : Exercise
                 
                 if (Map[nextPosition] == '#')
                 {
-                    return position;
+                    return (position, direction);
                 }
 
                 position = nextPosition;
             }
             
-            return position;
+            return (position, direction);
         }
-        
-        public IntVector2 AdvanceIfNecessaryOnCube(IntVector2 position, Direction direction, int distance)
+
+        public delegate (IntVector2 position, Direction direction) PositionTransformer(IntVector2 position,
+            Direction direction, int faceSize);
+
+        public (IntVector2, Direction) AdvanceIfNecessaryOnCube(IntVector2 position, Direction direction, int distance,
+            IReadOnlyDictionary<(IntVector2, IntVector2), PositionTransformersParameters> positionTransformers)
         {
             if (distance == 0)
             {
-                return position;
+                return (position, direction);
             }
 
             for (int i = 0; i < distance; i++)
             {
                 IntVector2 nextPosition = position + direction.Offset;
-                if (direction.Offset.X != 0)
+
+                IntVector2 faceIndices = position / _faceSize;
+                IntVector2 nextFaceIndices = (nextPosition + new IntVector2(_faceSize, _faceSize)) / _faceSize - new IntVector2(1, 1);
+
+                Direction nextDirection;
+                if (nextFaceIndices != faceIndices)
                 {
-                    if (nextPosition.X < HorizontalRange[nextPosition.Y].Start)
+                    if (!positionTransformers.TryGetValue((faceIndices, nextFaceIndices), out var parameters))
                     {
-                        (nextPosition, direction) = HandleTransition(position, nextPosition);
+                        throw new Exception($"Need transition from {faceIndices} to {nextFaceIndices}");
                     }
-                    else if (nextPosition.X > HorizontalRange[nextPosition.Y].End)
-                    {
-                        nextPosition = new IntVector2(HorizontalRange[nextPosition.Y].Start, nextPosition.Y);
-                    }
+
+                    nextPosition = parameters.FacePositionTransformer(position, direction, faceIndices, _faceSize);
+                    nextDirection = parameters.EntranceDirection;
                 }
                 else
                 {
-                    if (nextPosition.Y < VerticalRange[nextPosition.X].Start)
-                    {
-                        nextPosition = new IntVector2(nextPosition.X, VerticalRange[nextPosition.X].End);
-                    }
-                    else if (nextPosition.Y > VerticalRange[nextPosition.X].End)
-                    {
-                        nextPosition = new IntVector2(nextPosition.X, VerticalRange[nextPosition.X].Start);
-                    }
+                    nextDirection = direction;
                 }
                 
                 if (Map[nextPosition] == '#')
                 {
-                    return position;
+                    return (position, direction);
                 }
 
+                _pathChars[position] = direction.Char;
+                _pathChars[nextPosition] = 'x';
+                
                 position = nextPosition;
+                direction = nextDirection;
             }
             
-            return position;
-        }
-
-        private (IntVector2 nextPosition, Direction direction) HandleTransition(IntVector2 position, IntVector2 nextPosition)
-        {
-            //   012
-            // 0  XX
-            // 1  x
-            // 2 xx
-            // 3 x
-
-            IntVector2 facePos = position / _faceSize;
-            IntVector2 nextFacePos = nextPosition / _faceSize;
-
-            if (facePos == new IntVector2(1, 0))
-            {
-                if (nextFacePos == new IntVector2(0, 0))
-                { //0, 2, left
-                    return (new IntVector2(0, _faceSize * 3 - 1 - nextPosition.Y), Direction.Right);
-                }
-
-                // 1, -1 => 0, 3 left
-                return (new IntVector2(0, _faceSize * 3 + nextPosition.X - _faceSize), Direction.Right);
-            }
-
-            if (facePos == new IntVector2(2, 0))
-            {
-                if (nextPosition == new IntVector2(2, 1))
-                {
-                    // 1, 1, Right
-                    return (new IntVector2(2 * _faceSize - 1, nextPosition.X - 2 * _faceSize + _faceSize),
-                            Direction.Left);
-                }
-
-                if (nextPosition == new IntVector2(2, -1))
-                {
-                    // 0, 3, bottom
-                    // return (new IntVector2())
-                }
-                
-                // 3, 0
-                return (new IntVector2(2 * _faceSize - 1, 3 * _faceSize - 1 - nextPosition.Y), Direction.Left);
-
-            }
-
-            return (position, Direction.Bottom);
-
+            return (position, direction);
         }
 
         public static World Parse(string[] lines, int faceSize)
